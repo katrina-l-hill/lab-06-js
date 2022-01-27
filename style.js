bcc8f311daf826365366682ff6be5bbd29ab0d05
Katrina Hill <khill365@gmail.com>
2022-01-27 05:37:07 +0000

    updated code

--- style.js
+++ style.js
@@ -1,1 +1,13 @@
-let sign = prompt("Do you like ice cream?");
+let firstName = prompt("What is your name?");
+let sign = prompt("What is your quest?");
+let iceCreamPrompt = prompt("Do you like ice cream? (y or n)");
+if(iceCreamPrompt=="y")
+{
+    alert('Hello ' + firstName + ' Monty Python\'s flying cricus is the best ' + sign + '. You like ice cream.');
+}
+else
+{
+    alert('Hello ' + firstName + ' Monty Python\'s flying cricus is the best ' + sign + '. You do not like ice cream.');
+}
+
+document.write('<h3>' + sign + '</h3>');
