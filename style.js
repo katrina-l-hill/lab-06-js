624d6599e30d2cc131e381d6b140ba27d59f1ffd
Katrina Hill <khill365@gmail.com>
2022-01-27 22:41:19 +0000

    added .

--- style.js
+++ style.js
@@ -3,11 +3,11 @@ let sign = prompt("What is your quest?");
 let iceCreamPrompt = prompt("Do you like ice cream? (y or n)");
 if(iceCreamPrompt=="y")
 {
-    alert('Hello ' + firstName + ' Monty Python\'s flying cricus is the best ' + sign + '. You like ice cream.');
+    alert('Hello ' + firstName + ' Monty Python\'s Flying circus is the best ' + sign + '. You like ice cream.');
 }
 else
 {
-    alert('Hello ' + firstName + ' Monty Python\'s flying cricus is the best ' + sign + '. You do not like ice cream.');
+    alert('Hello ' + firstName + ' Monty Python\'s Flying circus is the best ' + sign + '. You do not like ice cream.');
 }
 
 document.write('<h3>' + sign + '</h3>');
